054f95597ba161728fcf88bb9a1de5b4b5f1dd69
Stefanescu Mihai <mstefanescu09@gmail.com>
2022-03-03 10:38:56 +0200

    .

---
+++ .config/dwm/scripts/autostart.sh
@@ -0,0 +1,19 @@
+#start dwmblocks
+dwmblocks &
+
+#load xresources
+xrdb ~/.config/x11/xresources &
+
+#start picom (compositor)
+picom --config ~/.config/picom/picom.conf &
+
+#start the sound server
+pipewire &
+/usr/bin/pipewire-pulse &
+/usr/bin/wireplumber &
+
+#set wallpaper
+setbg ~/.config/dwm/chill.png &
+
+#start dwm
+dwm
